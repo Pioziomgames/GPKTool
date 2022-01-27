5e52e4df1b5a956fccc123b59c1f7faa17fb28ec
minecraftpioziom <minecraftpioziom@gmail.com>
2022-01-27 19:33:32 +0100

    Add project files.

---
+++ GPKTool/GPK.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GpkFileSystem
+{
+    public class GPK
+    {
+        public List<GPKDeclaration> Declarations { get; set; }
+        public List<byte[]> Files { get; set; }
+
+        public GPK()
+        {
+            Declarations = new List<GPKDeclaration>();
+            Files = new List<byte[]>();
+        }
+        public GPK(string Path)
+        {
+            BinaryReader reader = new(File.Open(Path, FileMode.Open));
+            uint FileCount = reader.ReadUInt32();
+            Declarations = new();
+            Files = new List<byte[]>();
+            for (int i = 0; i < FileCount; i++)
+            {
+                Declarations.Add(new GPKDeclaration(reader));
+            }
+            for (int i = 0;i < FileCount; i++)
+            {
+               Files.Add(reader.ReadBytes(Declarations[i].Size));
+            }
+            reader.Close();
+        }
+
+        public GPK(BinaryReader reader)
+        {
+            uint FileCount = reader.ReadUInt32();
+            Declarations = new();
+            Files = new List<byte[]>();
+            for (int i = 0; i < FileCount; i++)
+            {
+                Declarations.Add(new GPKDeclaration(reader));
+            }
+            for (int i = 0; i < FileCount; i++)
+            {
+                Files.Add(reader.ReadBytes(Declarations[i].Size));
+            }
+        }
+
+        public void Save(string Path)
+        {
+            BinaryWriter writer = new(File.Open(Path, FileMode.Create));
+            writer.Write(Declarations.Count);
+            for (int i = 0; i < Declarations.Count; i++)
+                Declarations[i].Save(writer);
+            for (int i = 0; i < Files.Count; i++)
+                writer.Write(Files[i]);
+            writer.Close();
+        }
+
+        public void Save(BinaryWriter writer)
+        {
+            writer.Write(Declarations.Count);
+            for (int i = 0; i < Declarations.Count; i++)
+                Declarations[i].Save(writer);
+            for (int i = 0; i < Files.Count; i++)
+                writer.Write(Files[i]);
+        }
+
+    }
+    public class GPKDeclaration
+    {
+        public string FileName { get; set; }
+        public uint Offset { get; set; }
+        public int Size { get; set; }
+        public GPKDeclaration()
+        {
+            FileName = "";
+        }
+        public GPKDeclaration(BinaryReader reader)
+        {
+            FileName = new string(reader.ReadChars(260)).Replace("\0", "");
+            Size = reader.ReadInt32();
+            Offset = reader.ReadUInt32();
+        }
+        public GPKDeclaration(string Path)
+        {
+            BinaryReader reader = new(File.Open(Path, FileMode.Open));
+            FileName = new string(reader.ReadChars(260)).Replace("\0", "");
+            Size = reader.ReadInt32();
+            Offset = reader.ReadUInt32();
+            reader.Close();
+        }
+        public void Save(string Path)
+        {
+            BinaryWriter writer = new(File.Open(Path, FileMode.Create));
+            for (int i = 0; i < FileName.Length; i++)
+                writer.Write(FileName[i]);
+            for (int i = 0; i < 260 - FileName.Length; i++)
+                writer.Write((byte)0);
+            writer.Write(Size);
+            writer.Write(Offset);
+            writer.Close();
+        }
+        public void Save(BinaryWriter writer)
+        {
+            for (int i = 0; i < FileName.Length; i++)
+                writer.Write(FileName[i]);
+            for (int i = 0; i < 260 - FileName.Length; i++)
+                writer.Write((byte)0);
+            writer.Write(Size);
+            writer.Write(Offset);
+        }
+    }
+
+
+
+
+
+}
---
+++ GPKTool/Program.cs
@@ -0,0 +1,119 @@
+﻿// See https://aka.ms/new-console-template for more information
+using GpkFileSystem;
+
+class Program
+{
+    
+
+    static void Main(string[] args)
+    {
+
+
+        string InputFile = "";
+
+
+        if (args.Length > 0)
+            InputFile = args[0];
+        else
+        {
+            Console.WriteLine($"GPKTool by Pioziomgames\n" +
+                $"Extracts contents of SkyArena's GPK files\n" +
+                $"(Only tested on the PC version of MATSURI CLIMAX)\n" +
+                $"Usage:\n" +
+                $"       GPKTool.exe InputFile (optional)OutputFolder\n" +
+                $"       GPKTool.exe InputFolder (optional)OutputFile");
+            Exit();
+        }
+        string path = $"{Path.GetDirectoryName(InputFile)}\\{Path.GetFileNameWithoutExtension(InputFile)}_extracted"; // deletes the extension from the filename and adds _extracted
+
+        if (args.Length > 1)
+            path = args[1];
+
+        if (File.Exists(InputFile))
+        {
+            Console.WriteLine($"Loading: {InputFile}...");
+
+
+            GPK Archive = new(InputFile);
+
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+
+            for (int i = 0; i < Archive.Files.Count; i++)
+            {
+                File.WriteAllBytes(path + "\\" + Archive.Declarations[i].FileName, Archive.Files[i]);
+            }
+            Console.WriteLine($"Extracted {Archive.Files.Count} files to {path}");
+
+            return;
+        }
+        else if (!Directory.Exists(InputFile))
+        {
+            Console.WriteLine($"\n{InputFile} does not exist");
+            Exit();
+        }
+
+        if (args.Length == 1)
+            path = InputFile + ".gpk";
+
+
+        Console.WriteLine($"Reading the contents of: {InputFile}...");
+
+        GPK NewArchive = new();
+
+        string[] files;
+
+        string[] ddsFiles = Directory.GetFiles(InputFile, "*.dds", SearchOption.TopDirectoryOnly);
+
+        string[] wavFiles = Directory.GetFiles(InputFile, "*.wav", SearchOption.TopDirectoryOnly);
+
+        string[] momFiles = Directory.GetFiles(InputFile, "*.mom", SearchOption.TopDirectoryOnly);
+
+        string[] efcFiles = Directory.GetFiles(InputFile, "*.efc", SearchOption.TopDirectoryOnly);
+
+        string[] mdlFiles = Directory.GetFiles(InputFile, "*.mdl", SearchOption.TopDirectoryOnly);
+
+        string[] oggFiles = Directory.GetFiles(InputFile, "*.ogg", SearchOption.TopDirectoryOnly);
+
+        files = ddsFiles.Concat(wavFiles).Concat(momFiles).Concat(efcFiles).Concat(mdlFiles).Concat(oggFiles).ToArray();
+
+        Console.WriteLine($"{files.Length} files found");
+
+        uint offset = 4 + 268*(uint)files.Length;
+
+
+        for (int i =0; i < files.Length; i++)
+        {
+            byte[] file = File.ReadAllBytes(files[i]);
+            NewArchive.Files.Add(file);
+            GPKDeclaration Dec = new();
+            Dec.FileName = Path.GetFileName(files[i]);
+            Dec.Size = file.Length;
+            Dec.Offset = offset;
+            offset += (uint)Dec.Size;
+            NewArchive.Declarations.Add(Dec);
+        }
+
+        Console.WriteLine($"Saving to: {path}...");
+        NewArchive.Save(path);
+
+    }
+
+    static void Exit()
+    {
+        Console.WriteLine("\nPress Any Key to Quit");
+        Console.ReadKey();
+        System.Environment.Exit(0);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
